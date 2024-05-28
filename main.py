93d4719707d1e4b391569303c79d867bcc1a0b56
David <david-b-1@live.be>
2024-05-28 14:53:49 +0200

    update code

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from datetime import datetime
 
 def hello_world(request):
-    with open('logs.txt', 'rw') as f:
-        log = f"Hello World was invoked at {datetime.now()}\n"
-        f.write(log)
+    
     
     return "hello, world"
