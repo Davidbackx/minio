ef641aaffdea2f9c2df82b16c30a5beb78d7acf4
David <david-b-1@live.be>
2024-06-17 16:26:56 +0200

    added minio operator

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-
 EXPOSE 8080
 
 CMD ["python", "/app/main.py"]
--- main.py
+++ main.py
@@ -1,3 +1,52 @@
+from minio import Minio
+from minio.sse import SseCustomerKey
+from datetime import datetime
+import io
+
+client = Minio(
+    "localhost:9000",
+    access_key="sQTWYbkJoNcF9qH764Je",
+    secret_key="IgJTU0NLuzm8xyTNsvrmyw3JFGzQNdpfaw5ORUyV",
+    secure=False,
+)
+
+def set_object():
+    bucket_name = "testbucket"
+    object_key = "log.txt"
+
+    try:
+        response = client.get_object(bucket_name, object_key)
+        current_content = response.read().decode('utf-8')
+
+    finally:
+        response.close()
+        response.release_conn()
+
+    timestamp = datetime.now().isoformat()
+    new_log_entry = f"log-{timestamp}\n"
+    updated_content = current_content + new_log_entry
+
+    client.put_object(
+        bucket_name,
+        object_key,
+        io.BytesIO(updated_content.encode('utf-8')),
+        len(updated_content),
+        content_type='text/plain'
+    )
+    
+    data = client.get_object(bucket_name, object_key).read()
+    return data
+
+
+def get_object():
+    try:
+        response = client.get_object("testbucket", "log.txt")
+        print(response.read())
+    # Read data from response.
+    finally:
+        response.close()
+        response.release_conn()    
+
 def hello_world(request):
     if request.is_json:
         print(request.json)
@@ -5,15 +54,15 @@ def hello_world(request):
         print(request.data)
         print(request.form)
         
-    # write to log
-    with open("/tmp/log.txt", "a") as f:
-        f.write("Hello world\n")
-    
-
+    # write to log    
     print("Hello world")
     return "hello world"
 
 def get_logs(request):
     with open("/tmp/log.txt", "r") as f:
         return f.read()
+    
+    
+if __name__ == "__main__":
+    print(set_object())
     
